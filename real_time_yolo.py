1cdad89f8929bf865d9b897bd8ab50e79b187886
MiracDeveloper <91589543+MiracDeveloper@users.noreply.github.com>
2022-11-29 19:05:42 +0300

    real_time_yolo.py
    real_time_yolo.py

--- real_time_yolo.py
+++ real_time_yolo.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import time
 # Load Yolo
-net = cv2.dnn.readNet("weights/yolo-tiny.weights", "cfg/yolo-tiny.cfg")
+net = cv2.dnn.readNet("weights/yolov3-tiny.weights", "cfg/yolov3-tiny.cfg")
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
